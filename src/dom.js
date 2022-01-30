b260d344f65f7a6199c227e92dd945dce059c247
Kevin McPeak <kevmc46@gmail.com>
2022-01-30 15:19:38 -0500

    added basic todo functionality

--- src/dom.js
+++ src/dom.js
@@ -4,6 +4,7 @@ import {addProject} from './addProject';
 let dom = (() => {
 
     let projectList = document.querySelector("#project-list");
+    let todoList = document.querySelector("#todo-section");
 
     let createProjectDiv = (title, id) => {
         let div = document.createElement('div');
@@ -23,6 +24,39 @@ let dom = (() => {
         return div;
     };
 
+    let createToDoDiv = (task, description,duedate,priority,complete) => {
+        let todoDiv = document.createElement('div');
+        todoDiv.classList.add("todo-div");
+        
+        
+        let todoTask = document.createElement('div')
+        let todoDescription = document.createElement('div')
+        let todoDueDate = document.createElement('div')
+        let todoPriority = document.createElement('div')
+        let todoComplete = document.createElement('div');
+
+        let todoInformationArray = [todoDescription,todoDueDate,todoPriority,todoComplete];
+
+        todoInformationArray.forEach(item => {
+            item.classList.add("todoInfo");
+            todoDiv.appendChild(item);
+        });
+
+        todoTask.textContent = task;
+        todoDescription.textContent = description;
+        todoDueDate.textContent = duedate;
+        todoPriority.textContent = priority;
+        todoComplete.textContent = complete;
+
+        return todoDiv;
+        
+    };
+
+
+    let appendToDoDiv = (div) => {
+        todoList.appendChild(div);
+    }
+
     let appendProjectDiv = (div) => {
 
         projectList.appendChild(div);
@@ -37,9 +71,12 @@ let dom = (() => {
     let render = () => {    
         clearPage(projectList);
         addProject.mainArr.forEach(project => {
-            dom.appendProjectDiv(dom.createProjectDiv(project.title, project.id))
-        })
-        
+            dom.appendProjectDiv(dom.createProjectDiv(project.title, project.id));
+            project.tasks.forEach(task => {
+                appendToDoDiv(createToDoDiv(task.task,task.description, task.duedate, task.priority,task.completed));
+            })
+        });
+ 
     };
 
     return {createProjectDiv, appendProjectDiv, render}
